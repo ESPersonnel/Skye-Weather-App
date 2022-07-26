46214dca6da6ea99d8b66a0ee6df8a078a21e4c5
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-07-26 15:12:11 +0300

    Added API key for openweathermap

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e9629fd62cmshe9a00d53c3d3a85p1b9e7fjsn82fd2c24afbe',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "834b35552fcfb0c0071704a4c517816d";
