bae4a9c186e262382dfa534413b14980102d3be4
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-07-26 07:23:48 +0300

    Added search foption to search a city based on population >1000000

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,8 +1,29 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from "../../api";
 
 const Search = ({ onSearchChange }) => {
     const [search, setSearch] = useState(null);
+
+    const loadOptions = (inputValue) => {
+        return fetch(
+          `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+          geoApiOptions
+        )
+          .then((response) => response.json())
+          .then((response) => {
+            return {
+              options: response.data.map((city) => {
+                return {
+                  value: `${city.latitude} ${city.longitude}`,
+                  label: `${city.name}, ${city.countryCode}`,
+                };
+              }),
+            };
+          });
+      };
+
+
 
     const handleOnChange = (value) => {
         setSearch(value);
@@ -12,9 +33,10 @@ const Search = ({ onSearchChange }) => {
     return (
         <AsyncPaginate 
         placeholder="Search for city"
-        debounceTimeout={300}
+        debounceTimeout={600}
         value={search}
         onChange={handleOnChange}
+        loadOptions={loadOptions}
         />
     );
 }
