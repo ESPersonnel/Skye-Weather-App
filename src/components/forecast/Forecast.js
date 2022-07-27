f58caeefbb1b19fa75c01599bb2de938aa5d033a
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-07-27 23:17:20 +0300

    Rerouting css source

--- src/components/forecast/Forecast.js
+++ src/components/forecast/Forecast.js
@@ -6,7 +6,7 @@ import {
   AccordionItemButton,
   AccordionItemPanel,
 } from "react-accessible-accordion";
-import "./Forecast.css";
+import "../../App.css";
 
 const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
@@ -16,14 +16,14 @@ const Forecast = ({ data }) => {
   
   return (
     <>
-      <label className="title">Daily</label>
+      <label className="title">Forecast</label>
       <Accordion allowZeroExpanded>
         {data.list.splice(0, 7).map((item, idx) => (
           <AccordionItem key={idx}>
             <AccordionItemHeading>
               <AccordionItemButton>
                 <div className="daily-item">
-                  <img src={`icons/${item.weather[0].icon}.png`} className="icon-small" alt="weather" />
+                  <img src={`https://openweathermap.org/img/wn/${item.weather[0].icon}.png`} className="icon-small" alt="weather" />
                   <label className="day">{forecastDays[idx]}</label>
                   <label className="description">{item.weather[0].description}</label>
                   <label className="min-max">{Math.round(item.main.temp_max)}°C /{Math.round(item.main.temp_min)}°C</label>
