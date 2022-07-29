337a6c876ec0b95d6548625fec803528c24585c6
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-07-29 16:35:40 +0300

    Adding a title

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../App.css"
+
+function Title() {
+  return (
+    <div className='title-card1'>
+        <div className='title1'>Skye Weather</div>
+    </div>
+  )
+}
+
+export default Title
